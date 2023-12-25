a446c0438528a5410deaa6d665d96439a3df0d12
Adarsh S <adarshsunil@gmail.com>
2023-12-25 23:02:05 +0530

    Update Transaction.py

--- Code/ProofOfImmune/Transaction.py
+++ Code/ProofOfImmune/Transaction.py
@@ -5,11 +5,12 @@ import copy
 
 class Transaction():
 
-    def __init__(self, senderPublicKey, receiverPublicKey, cp, type):
+    def __init__(self, senderPublicKey, receiverPublicKey, karma_score, social_rep, type):
         self.senderPublicKey = senderPublicKey
         self.receiverPublicKey = receiverPublicKey
-        self.cp = cp
-        self.type = type
+        self.karma_score = karma_score
+        self.social_rep = social_rep
+        self.type = type 
         self.id = (uuid.uuid1()).hex
         self.timestamp = time.time()
         self.signature = ''
@@ -28,8 +29,6 @@ class Transaction():
     def danger_score(karma_score, social_rep):
         danger = ((1-karma_score)*(1-social_rep))
         return danger
-
-   
 
     def equals(self, transaction):
         if self.id == transaction.id:
